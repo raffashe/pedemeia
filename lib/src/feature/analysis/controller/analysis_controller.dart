3e1717c3345589aef615ed46c70d98b9f5bbb784
Raffaela de Castro <raffaela.castro@pixforce.ai>
2024-02-20 23:54:30 -0300

    feat: :rocket: init pe de meia
    init pe de meia
    
    init pe de meia

---
+++ lib/src/feature/analysis/controller/analysis_controller.dart
@@ -0,0 +1,16 @@
+import '../model/analysis_controller.dart';
+
+class MeuController {
+  final MeuModel modelo;
+
+  MeuController({required this.modelo});
+
+  void atualizarModelo() {
+    
+  }
+
+  void processarEntrada(String entrada) {
+    
+  }
+}
+
---
+++ lib/src/feature/analysis/view/analysis_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+import '../controller/analysis_controller.dart';
+
+class MinhaPagina extends StatelessWidget {
+  final MeuController controller;
+
+  const MinhaPagina({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Título da Página'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Exemplo de Visualização',
+            ),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.processarEntrada('alguma entrada');
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
